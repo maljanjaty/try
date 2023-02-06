c4ad3836e4e9aee268555673834cbfaa8d7971cf
Katsiaryna Tsarova <ktararyna@ibagroup.eu>
2023-02-06 07:11:51 +0000

    Updates in package.json and app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,32 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
 
+const app = express();
+const path = require('path');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
-app.use( express.static( "public" ) );
-
+app.use(express.static('public'));
 
 // prepare server
 
-app.use('/', express.static(__dirname + '/www')); // redirect root
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/', express.static(`${__dirname}/www`)); // redirect root
+app.use('/js', express.static(`${__dirname}/node_modules/bootstrap/dist/js`)); // redirect bootstrap JS
+app.use('/js', express.static(`${__dirname}/node_modules/jquery/dist`)); // redirect JS jQuery
+app.use('/css', express.static(`${__dirname}/node_modules/bootstrap/dist/css`)); // redirect CSS bootstrap
 
 // index page
-app.get('/', function(req, res) {
-    res.render('page/index');
+app.get('/', (req, res) => {
+  res.render('page/index');
+});
+
+
+  app.listen(5000, () {
+  console.log('App listening on port 5000!');
 });
 
 
 
-  //app.listen(5000, function () {
- // console.log('App listening on port 5000!');
- // });
+
+
+
